a90424adfbb81ada05cff2b2582a4f30561f2674
Jasper13006 <jasper13006@gmail.com>
2021-06-01 13:23:01 -0500

    feat: mongo deleted as database

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,6 @@ import { config } from "dotenv";
 config();
 
 export default {
-  MONGO_DATABASE: process.env.MONGO_DATABASE || 'albo-heroes',
-  MONGO_USER: process.env.MONGO_USER || "admin",
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD || "admin",
-  MONGO_HOST: process.env.MONGO_HOST || 'localhost',
   PORT: process.env.PORT || 4000,
   MARVEL_API: process.env.MARVEL_API || "http://gateway.marvel.com/v1/public",
   PRIVATE_KEY: process.env.PRIVATE_KEY ,
--- src/database.ts
+++ src/database.ts
@@ -1,19 +1,5 @@
-import mongoose, { ConnectionOptions } from 'mongoose'
 import config from './config'
 import firebaseAdmin from 'firebase-admin';
-(async () => {
-    try {
-        const mongooseOptions: ConnectionOptions = {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-
-        }
-        const db = await mongoose.connect(`mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`, mongooseOptions)
-        console.log(`database is connected to: ${db.connection.name}`)
-    } catch (err) {
-        console.error(err)
-    }
-})()
 
 const admin = firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert(`${config.FIREBASE_CREDENTIALS}`),
