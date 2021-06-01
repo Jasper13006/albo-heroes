000589fb45c69fd75c747a22e57894c2a5dd904f
Jasper13006 <jasper13006@gmail.com>
2021-06-01 16:04:55 -0500

    feat: excecute populate functions when the server  is iniciated

--- src/modules/integrationMarvelApi/cron/index.ts
+++ src/modules/integrationMarvelApi/cron/index.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { getInformationOfCharacter } from '../service';
 
-cron.schedule('0 1 * * *', async () => {
+const task = cron.schedule('0 1 * * *', async () => {
     console.info('Running a character sync at 01:00 at America/Mexico_City timezone');
     await getInformationOfCharacter('ironman')
     await getInformationOfCharacter('capamerica')
@@ -9,3 +9,8 @@ cron.schedule('0 1 * * *', async () => {
     scheduled: true,
     timezone: "America/Mexico_City"
 });
+(async()=>{
+    await getInformationOfCharacter('ironman')
+    await getInformationOfCharacter('capamerica')
+    task.start()
+})()
