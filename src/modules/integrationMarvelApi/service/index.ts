0bbc7a1828bef983a9e6f2a9a08ec1bac33aa976
Jasper13006 <jasper13006@gmail.com>
2021-06-01 07:59:32 -0500

    feat: create funtions for create requests

--- src/modules/integrationMarvelApi/service/index.ts
+++ src/modules/integrationMarvelApi/service/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import * as admin from 'firebase-admin';
 import db from '../../../database';
 import config from '../../../config'
-import {hasher,makeChunks} from '../../../utils/index';
+import { hasher, makeChunks } from '../../../utils/index';
 
 //generar creador de peticiones a la api
 //probar que le pueda pegar la cantidad de peticiones necesarias
@@ -15,20 +15,33 @@ const ts: number = Date.now()
 const hash: string = hasher([String(ts), config.PRIVATE_KEY, config.PUBLIC_KEY])
 const auth: string = `apikey=${config.PUBLIC_KEY}&ts=${ts}&hash=${hash}`
 
-const generateMarvelApiRequest = async(characterId:number)=>{
+const createRequestCollection = async (characterId: number) => {
+    console.time("first")
     const comicsResponse = await axios.get(`${config.MARVEL_API}/characters/${characterId}/comics?limit=1&${auth}`)
-    const nPages:number = Math.ceil(comicsResponse.data.data.total/100)
-    const arrayOfRequests = [...Array(2).keys()].map((e:number)=>{
-        const skipAndLimit = `limit=100&offset=${100*e}`
-        return axios.get(`${config.MARVEL_API}/characters/${characterId}/comics?${skipAndLimit}&${auth}`)
+    console.timeEnd("first")
+    const nPages: number = Math.ceil(comicsResponse.data.data.total / 100)
+    const arrayOfRequests = [...Array(nPages).keys()].map((e: number) => {
+        const skipAndLimit = `limit=100&offset=${100 * e}`
+        const url = `${config.MARVEL_API}/characters/${characterId}/comics?${skipAndLimit}&${auth}`
+        return createRequestPromise(url)
     })
 
-    const chunks = makeChunks(arrayOfRequests,2)
-    console.log(chunks)
     return arrayOfRequests;
 }
-  
 
+const createRequestPromise = (url: string) => {
+    let promise = new Promise(async (resolve, reject) => {
+        try {
+            console.time(url)
+            const comicResponse = await axios.get(url)
+            console.timeEnd(url)
+            resolve(comicResponse)
+        } catch (err) {
+            reject(err)
+        }
+    })
+    return promise
+}
 
 export const getInformationOfCharacter = async (characterName: string): Promise<void> => {
     if (!config.PRIVATE_KEY || !config.PUBLIC_KEY) {
@@ -39,27 +52,29 @@ export const getInformationOfCharacter = async (characterName: string): Promise<
     // const auth: string = `apikey=${config.PUBLIC_KEY}&ts=${ts}&hash=${hash}`
 
     const last_sync = new Date()
-
+    console.log("Llego aqui?")
     const characterResponse = await axios.get(`${config.MARVEL_API}/characters?name=Iron%20Man&${auth}`)
     const { name, id } = characterResponse.data.data.results[0]
-    const promises = await generateMarvelApiRequest(id)
-    console.log("Hola")
+    const promises = await createRequestCollection(id)
+    console.log(promises)
+    console.time("axiosResolve")
     const promisesResolves = await Promise.all(promises)
-    console.log(promisesResolves)
+    console.timeEnd("axiosResolve")
+    console.log("FINISH")
     //comprobar si el caracter existe en db
-    
+
     // console.log(id)
     // let characterRef = db.collection('character').doc(`${id}`);
     // let character = await characterRef.get()
 
     // if (!character.exists) {
-        // const dataToCreate = {
-            // name,
-            // id,
-            // last_sync,
-            // colaborators:{ writers:[], editors:[], colorists:[] }
-        // }
-        // const newcharacter = await characterRef.set(dataToCreate)
+    // const dataToCreate = {
+    // name,
+    // id,
+    // last_sync,
+    // colaborators:{ writers:[], editors:[], colorists:[] }
+    // }
+    // const newcharacter = await characterRef.set(dataToCreate)
     // }
     // const comicsResponse = await axios.get(`${config.MARVEL_API}/characters/1009368/comics?limit=1&${auth}`)
     // const writers: string[] = []
@@ -67,19 +82,19 @@ export const getInformationOfCharacter = async (characterName: string): Promise<
     // const colorists: string[] = []
 
     // comicsResponse.data.data.results.forEach((element: any) => {
-        // const { id, title, creators, characters } = element
-        // creators.items.forEach((e: any) => {
-            // if (e.role === 'editor') editors.push(e.name)
-            // if (e.role === 'writer') writers.push(e.name)
-            // if (e.role === 'colorist') colorists.push(e.name)
-        // });
+    // const { id, title, creators, characters } = element
+    // creators.items.forEach((e: any) => {
+    // if (e.role === 'editor') editors.push(e.name)
+    // if (e.role === 'writer') writers.push(e.name)
+    // if (e.role === 'colorist') colorists.push(e.name)
+    // });
     // });
     // const setWriters = new Set(writers)
     // console.log(setWriters)
     // const res = await characterRef.update({
-        // writers:[...setWriters],
-        // editors:admin.firestore.FieldValue.arrayUnion(editors),
-        // colorists:admin.firestore.FieldValue.arrayUnion(colorists)
+    // writers:[...setWriters],
+    // editors:admin.firestore.FieldValue.arrayUnion(editors),
+    // colorists:admin.firestore.FieldValue.arrayUnion(colorists)
     // });
     // console.log({ writers, editors, colorists })
     return
