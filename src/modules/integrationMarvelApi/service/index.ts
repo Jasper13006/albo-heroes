04fb53167479f7c8d6fe026ddf95158024eec5bb
Jasper13006 <jasper13006@gmail.com>
2021-06-01 10:38:03 -0500

    feat: add function to merge all pages

--- src/modules/integrationMarvelApi/service/index.ts
+++ src/modules/integrationMarvelApi/service/index.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin';
 import db from '../../../database';
 import config from '../../../config'
 import { hasher } from '../../../utils/index';
+import { IpageOfComics } from '../../../interface';
 
 if (!config.PRIVATE_KEY || !config.PUBLIC_KEY) {
     throw new Error(`Nonexistent marvel credentials`)
@@ -11,57 +12,6 @@ const ts: number = Date.now()
 const hash: string = hasher([String(ts), config.PRIVATE_KEY, config.PUBLIC_KEY])
 const auth: string = `apikey=${config.PUBLIC_KEY}&ts=${ts}&hash=${hash}`
 
-const createRequestCollection = async (characterId: number) => {
-    const comicsResponse = await axios.get(`${config.MARVEL_API}/characters/${characterId}/comics?limit=1&${auth}`)
-    const nPages: number = Math.ceil(comicsResponse.data.data.total / 100)
-    const arrayOfRequests = [...Array(1).keys()].map((e: number) => {
-        const skipAndLimit = `limit=100&offset=${100 * e}`
-        const url = `${config.MARVEL_API}/characters/${characterId}/comics?${skipAndLimit}&${auth}`
-        return createRequestPromise(url)
-    })
-
-    return arrayOfRequests;
-}
-
-const createRequestPromise = (url: string) => {
-    let promise = new Promise(async (resolve, reject) => {
-        try {
-            const comicResponse = await axios.get(url)
-            const results = comicResponse.data.data.results
-            const finallyResult = cleanComics(results)
-            resolve(finallyResult)
-        } catch (err) {
-            reject(err)
-        }
-    })
-    return promise
-}
-
-const cleanComics = (results: any) => {
-    const writers = new Set()
-    const editors = new Set()
-    const colorists = new Set()
-    let charactersToSend: any[] = []
-
-    results.forEach((element: any) => {
-        const { id, title, creators, characters } = element
-        creators.items.forEach((e: any) => {
-            if (e.role === 'editor') editors.add(e.name)
-            if (e.role === 'writer') writers.add(e.name)
-            if (e.role === 'colorist') colorists.add(e.name)
-        });
-        characters.items.forEach((character: any) => {
-            const { name, resourceURI } = character
-            const idCharacter = resourceURI.split('/').pop()
-            charactersToSend.push({
-                name,
-                id: idCharacter,
-                comics: [title]
-            })
-        })
-    });
-    console.log(charactersToSend)
-}
 export const getInformationOfCharacter = async (characterName: string): Promise<void> => {
     const last_sync = new Date()
 
@@ -73,6 +23,12 @@ export const getInformationOfCharacter = async (characterName: string): Promise<
     const promisesResolves = await Promise.all(promises)
     console.timeEnd("axiosResolve")
     console.log("FINISH")
+
+    //* merge all pages
+    console.time("MergeAllInfoo")
+    const allInfoOfCharacter = mergeAllPages(promisesResolves)
+    console.timeEnd("MergeAllInfoo")
+    // console.log(allInfoOfCharacters)
     //comprobar si el caracter existe en db
 
     // console.log(id)
@@ -93,3 +49,80 @@ export const getInformationOfCharacter = async (characterName: string): Promise<
     return
 }
 
+const mergeAllPages = (pagesOfComics:IpageOfComics[]):IpageOfComics | void =>{
+    const mergedPages = pagesOfComics.reduce((allPages,nextPage):IpageOfComics=>{
+        return {
+            writers:[...allPages.writers,...nextPage.writers],
+            editors:[...allPages.editors,...nextPage.editors],
+            colorists:[...allPages.colorists,...nextPage.colorists],
+            characters:[...allPages.characters,...nextPage.characters],
+        }
+    })
+    const writers: Set<string> = new Set(mergedPages.writers)
+    const editors: Set<string> = new Set(mergedPages.editors)
+    const colorists: Set<string> = new Set(mergedPages.colorists)
+    return {
+        characters:mergedPages.characters,
+        writers:[...writers],
+        editors:[...editors],
+        colorists:[...colorists],
+    }
+}
+
+
+const createRequestCollection = async (characterId: number) => {
+    const comicsResponse = await axios.get(`${config.MARVEL_API}/characters/${characterId}/comics?limit=1&${auth}`)
+    const nPages: number = Math.ceil(comicsResponse.data.data.total / 100)
+    const arrayOfRequests = [...Array(nPages).keys()].map((e: number) => {
+        const skipAndLimit = `limit=100&offset=${100 * e}`
+        const url = `${config.MARVEL_API}/characters/${characterId}/comics?${skipAndLimit}&${auth}`
+        return createRequestPromise(url)
+    })
+    return arrayOfRequests;
+}
+
+const createRequestPromise = (url: string):Promise<IpageOfComics> => {
+    let promise:Promise<IpageOfComics> = new Promise(async (resolve, reject) => {
+        try {
+            const comicResponse = await axios.get(url)
+            const results = comicResponse.data.data.results
+            const finallyResult = cleanOnePageOfComics(results)
+            resolve(finallyResult)
+        } catch (err) {
+            reject(err)
+        }
+    })
+    return promise
+}
+
+const cleanOnePageOfComics = (results: any): IpageOfComics => {
+    const writers: Set<string> = new Set()
+    const editors: Set<string> = new Set()
+    const colorists: Set<string> = new Set()
+    let charactersToSend: any[] = []
+
+    results.forEach((element: any) => {
+        const { title, creators, characters } = element
+        creators.items.forEach((e: any) => {
+            if (e.role === 'editor') editors.add(String(e.name))
+            if (e.role === 'writer') writers.add(String(e.name))
+            if (e.role === 'colorist') colorists.add(String(e.name))
+        });
+        characters.items.forEach((character: any) => {
+            const { name, resourceURI } = character
+            const idCharacter = resourceURI.split('/').pop()
+            charactersToSend.push({
+                name,
+                id: idCharacter,
+                comics: [title]
+            })
+        })
+    });
+    const pageOfComics = {
+        writers: [...writers],
+        editors: [...editors],
+        colorists: [...colorists],
+        characters: charactersToSend
+    }
+    return pageOfComics
+}
