9241abe80f3e1de9c6cdde6092972c799844ac84
Jasper13006 <jasper13006@gmail.com>
2021-06-01 15:20:16 -0500

    fix: delete console logs

--- src/modules/integrationMarvelApi/repository/index.ts
+++ src/modules/integrationMarvelApi/repository/index.ts
@@ -7,11 +7,9 @@ export const createOrUpdate = async(characterData: ICharacter)=>{
         const characterRef = db.collection('characters').doc(`${characterData.id}`)
         const existCharacter = await characterRef.get()
         if(!existCharacter.exists){
-            console.log("No Existo")
             const created = await characterRef.set(characterData);
             return created.writeTime;
         }else{
-            console.log("existo")
             const updated = await characterRef.update(characterData);
             return updated.writeTime;
         }
