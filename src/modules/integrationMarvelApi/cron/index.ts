958b8621c08f45ff8891349c529cfbccb35f89f8
Jasper13006 <jasper13006@gmail.com>
2021-06-01 21:07:36 -0500

    fix: a little fix in promises

--- src/modules/integrationMarvelApi/cron/index.ts
+++ src/modules/integrationMarvelApi/cron/index.ts
@@ -3,14 +3,16 @@ import { getInformationOfCharacter } from '../service';
 
 const task = cron.schedule('0 1 * * *', async () => {
     console.info('Running a character sync at 01:00 at America/Mexico_City timezone');
-    await getInformationOfCharacter('ironman')
-    await getInformationOfCharacter('capamerica')
+    await Promise.all([ getInformationOfCharacter('ironman'),
+                        getInformationOfCharacter('capamerica')])
 }, {
     scheduled: true,
     timezone: "America/Mexico_City"
 });
 (async()=>{
-    await getInformationOfCharacter('ironman')
-    await getInformationOfCharacter('capamerica')
+    console.time("Populated completed in: ")
+    await Promise.all([getInformationOfCharacter('ironman'),
+    getInformationOfCharacter('capamerica')])
+    console.timeEnd("Populated completed in: ")
     task.start()
 })()
